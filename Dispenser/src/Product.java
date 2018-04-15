f7efa4f844a9c5048d9c7a4031fa56286fb8b24f
Chase Hausman <chase@chaseh.net>
2018-04-15 00:14:05 -0700

    Some changes to absract Product class to be closer to ready to submit

--- Dispenser/src/Product.java
+++ Dispenser/src/Product.java
@@ -5,21 +5,24 @@ public abstract class Product {
 	private double price;
 	private int maxQuantity;
 	private int currentQuantity;
-	
+
+	// No-argument constructor, with terribly useless default values
 	protected Product() {
-		name = "";
+		name = "A New Product";
 		price = 0;
 		maxQuantity = 0;
 		currentQuantity = 0;
 	}
-	
+
+	// Constructor with setting new values
 	protected Product(String name, double price, int maxQuantity, int currentQuantity) {
 		this.name = name;
 		this.price = price;
 		this.maxQuantity = maxQuantity;
 		this.currentQuantity = currentQuantity;
 	}
-	
+
+	// Constructor that copies fields (if available) from a similar product
 	protected Product(Product input) {
 		name = input.name;
 		price = input.price;
@@ -57,5 +60,11 @@ public abstract class Product {
 	
 	public int getMaxQuantity() {
 		return maxQuantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Product Name\t" + getName() + "\nPrice\t\t" + getPrice() + "\nMax Quantity\t" + getMaxQuantity() +
+				"\nCurrent Quantity\t" + getCurrentQuantity();
 	}
 }
